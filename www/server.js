9815dbc6fc1925e40385df94bd173ede20211bca
MrFleck <felipefleck@live.com>
2021-06-15 20:02:47 -0300

    teste da ssh key

--- www/server.js
+++ www/server.js
@@ -9,7 +9,7 @@ const loginRouter = require('./routes/loginRouter');
 const cadastroRouter = require('./routes/cadastroRouter');
 const homeRouter = require('./routes/homeRouter');
 
-//CONFIG 
+//CONFIG  
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/public', express.static(__dirname + '/views/public/'));
